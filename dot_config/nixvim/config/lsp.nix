1d0e53929b0fe3774134cc0b5e46ff1fefd9ee51
arias <aria-tec@gmail.com>
2026-07-30 12:27:17 +0700

    feat: add Xcode Dark theme for nixvim, alacritty, zellij & optimize rust-analyzer LSP

--- dot_config/nixvim/config/lsp.nix
+++ dot_config/nixvim/config/lsp.nix
@@ -1,21 +1,35 @@
 {
   plugins.lsp = {
     enable = true;
-    # lazyLoad = {
-    #   enable = true;
-    #   settings = {
-    #     event = [
-    #       "BufReadPre"
-    #       "BufNewFile"
-    #     ];
-    #   };
-    # };
   };
+
   plugins.lsp.servers = {
     nil_ls.enable = true;
-    # tsserver.enable = true;
-    ts_ls.enable = true;
     vtsls.enable = true;
-    tsgo.enable = true;
+
+    rust_analyzer = {
+      enable = true;
+      installCargo = true;
+      installRustc = true;
+      settings = {
+        cargo = {
+          allFeatures = true;
+          buildScripts = {
+            enable = true;
+          };
+        };
+        check = {
+          command = "clippy";
+        };
+        procMacro = {
+          enable = true;
+        };
+        inlayHints = {
+          typeHints.enable = true;
+          parameterHints.enable = true;
+          chainingHints.enable = true;
+        };
+      };
+    };
   };
 }
