218d997ee72ce35d49a779127de88ab2c287fb6b
arias <aria-tec@gmail.com>
2026-08-03 20:10:55 +0700

    kanagawa

--- dot_config/nvim/plugin/40_plugins.lua
+++ dot_config/nvim/plugin/40_plugins.lua
@@ -171,54 +171,81 @@ end)
 Config.now(function()
   -- Install only those that you need
   add({
-    'https://github.com/navarasu/onedark.nvim',
+    -- 'https://github.com/navarasu/onedark.nvim',
     -- 'https://github.com/sainnhe/everforest',
     -- 'https://github.com/Shatur/neovim-ayu',
     -- 'https://github.com/ellisonleao/gruvbox.nvim',
+    'https://github.com/rebelot/kanagawa.nvim'
   })
 
-  require('onedark').setup({
-    -- Main options --
-    style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-    transparent = true, -- Show/hide background
-    term_colors = true, -- Change terminal color as per the selected theme style
-    ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-    cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+  -- require('onedark').setup({
+  --   -- Main options --
+  --   style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+  --   transparent = true, -- Show/hide background
+  --   term_colors = true, -- Change terminal color as per the selected theme style
+  --   ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+  --   cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+  --
+  --   -- toggle theme style ---
+  --   toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+  --   toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' }, -- List of styles to toggle between
+  --
+  --   -- Change code style ---
+  --   -- Options are italic, bold, underline, none
+  --   -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+  --   code_style = {
+  --     comments = 'italic',
+  --     keywords = 'none',
+  --     functions = 'none',
+  --     strings = 'none',
+  --     variables = 'none',
+  --   },
+  --
+  --   -- Lualine options --
+  --   lualine = {
+  --     transparent = true, -- lualine center bar transparency
+  --   },
+  --
+  --   -- Custom Highlights --
+  --   colors = {}, -- Override default colors
+  --   highlights = {}, -- Override highlight groups
+  --
+  --   -- Plugins Config --
+  --   diagnostics = {
+  --     darker = true, -- darker colors for diagnostic
+  --     undercurl = true, -- use undercurl instead of underline for diagnostics
+  --     background = true, -- use background color for virtual text
+  --   },
+  -- })
+  -- require('onedark').load()
 
-    -- toggle theme style ---
-    toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-    toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' }, -- List of styles to toggle between
-
-    -- Change code style ---
-    -- Options are italic, bold, underline, none
-    -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
-    code_style = {
-      comments = 'italic',
-      keywords = 'none',
-      functions = 'none',
-      strings = 'none',
-      variables = 'none',
-    },
-
-    -- Lualine options --
-    lualine = {
-      transparent = true, -- lualine center bar transparency
-    },
-
-    -- Custom Highlights --
-    colors = {}, -- Override default colors
-    highlights = {}, -- Override highlight groups
-
-    -- Plugins Config --
-    diagnostics = {
-      darker = true, -- darker colors for diagnostic
-      undercurl = true, -- use undercurl instead of underline for diagnostics
-      background = true, -- use background color for virtual text
-    },
+  -- Default options:
+  require('kanagawa').setup({
+      compile = false,             -- enable compiling the colorscheme
+      undercurl = true,            -- enable undercurls
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true},
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = true,         -- do not set background color
+      dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+      colors = {                   -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+      },
+      overrides = function(colors) -- add/modify highlights
+          return {}
+      end,
+      theme = "wave",              -- Load "wave" theme
+      background = {               -- map the value of 'background' option to a theme
+          dark = "wave",           -- try "dragon" !
+          light = "lotus"
+      },
   })
-  require('onedark').load()
 
-  -- vim.cmd('color everforest')
+  vim.cmd('color kanagawa')
 end)
 -- Startup time benchmark =====================================================
 -- Measures Neovim startup time across 100 iterations via `:StartupTime` command.
